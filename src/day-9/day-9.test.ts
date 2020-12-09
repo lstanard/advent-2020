3ecb08f39a473405b1b1f768af55b422a4400754
Charlie Stanard <louis.stanard@gmail.com>
2020-12-09 07:31:53 -0700

    Completed Day 9, Part 2

--- src/day-9/day-9.test.ts
+++ src/day-9/day-9.test.ts
@@ -2,6 +2,7 @@ import input from "./input";
 import sampleInput from "./sample-input";
 
 import { findNumberWithoutSum } from "./part-1";
+import { findContiguousSubsetAndSum } from "./part-2";
 
 describe("findNumberWithoutSum", () => {
   it("should return the correct value for sampleInput", () => {
@@ -10,5 +11,15 @@ describe("findNumberWithoutSum", () => {
 
   it("should return the correct value for input", () => {
     expect(findNumberWithoutSum(input)).toEqual(507622668);
+  });
+});
+
+describe("findContiguousSubsetAndSum", () => {
+  it("should return the correct value for sampleInput", () => {
+    expect(findContiguousSubsetAndSum(sampleInput, 5)).toEqual(62);
+  });
+
+  it("should return the correct value for input", () => {
+    expect(findContiguousSubsetAndSum(input)).toEqual(76688505);
   });
 });
---
+++ src/day-9/part-2.ts
@@ -0,0 +1,49 @@
+import input from "./input";
+import { findNumberWithoutSum } from "./part-1";
+
+/**
+ * Solution for Part 2 -
+ * Find a contiguous set of at least two numbers in your list which sum to the
+ * invalid number provided by findNumberWithoutSum, then return the sum of the
+ * smallest and largest numbers from that range.
+ *
+ * @param numbers
+ * @param preambleLength
+ */
+export function findContiguousSubsetAndSum(
+  numbers: number[],
+  preambleLength = 25
+): number {
+  const targetValue = findNumberWithoutSum(numbers, preambleLength);
+  let resultSet: number[] = [];
+  loop1: for (let i = 0; i < numbers.length; i++) {
+    if (
+      numbers[i] >= targetValue ||
+      numbers[i] + numbers[i + 1] > targetValue
+    ) {
+      break;
+    }
+    for (let j = i + 2; j < numbers.length; j++) {
+      const set = [...numbers].slice(i, j);
+      if (set.length) {
+        const sum = set.reduce((a, b) => a + b);
+        if (sum > targetValue) {
+          break;
+        } else {
+          if (sum === targetValue) {
+            resultSet = set;
+            break loop1;
+          }
+        }
+      }
+    }
+  }
+  return Math.min(...resultSet) + Math.max(...resultSet);
+}
+
+/**
+ * Executes in ~152ms, not as bad as I thought it might be
+ */
+console.time("findContiguousSubsetAndSum");
+console.log(findContiguousSubsetAndSum(input));
+console.timeEnd("findContiguousSubsetAndSum");
